a79a603211cf545b717a713389d84b07e41b235f
h671420 <671420@stud.hvl.no>
2024-09-27 15:38:57 +0200

    w39 nesten

---
+++ haskell/ukeoppgaver/Week39.hs
@@ -0,0 +1,65 @@
+
+module Week39 where
+import Distribution.Simple.Setup (falseArg)
+
+data Set a = Empty | Singleton a | Union (Set a) (Set a)
+    deriving (Eq, Show)
+
+{--
+unique :: Eq a => [a] -> [a]
+unique li 
+    | null li = li
+    | head li `isIn` tail li = unique (tail li)
+    | otherwise = head li:unique (tail li) where
+        isIn :: Eq a => a -> [a] -> Bool
+        isIn x xs
+            |null xs = False
+            |otherwise = x == head xs || isIn x (tail xs)  
+--}
+
+fromList :: [a] -> Set a
+fromList a
+    | null a = Empty
+    | null (tail a) = Singleton (head a)
+    | otherwise = Union (Singleton (head a)) (fromList (tail a))
+
+a = fromList [1,1,2]
+b = fromList [2,2,3]
+empt = fromList []
+zeros = fromList [0,0,0,0]
+
+isIn :: Eq a => a -> Set a -> Bool
+isIn a Empty = False
+isIn a (Singleton b) = a==b
+isIn a (Union b c ) = isIn a b || isIn a c
+
+
+subset :: Eq a => Set a -> Set a -> Bool
+subset Empty b = True
+subset (Singleton a) b = isIn a b
+subset (Union (Singleton a) c) b = isIn a b && subset c b
+
+setEq :: Eq a => Set a -> Set a -> Bool
+setEq a b =  subset a b && subset b a
+
+
+
+data Nat = Zero | Succ Nat
+    deriving (Show, Eq)
+
+
+foldNat :: a -> (a -> a) -> Nat -> a
+foldNat acc _ Zero = acc
+foldNat acc funk (Succ nat) = foldNat (funk acc) funk nat
+
+foldSet :: b -> (a -> b) -> (b -> b -> b) -> Set a -> b
+foldSet acc _ _ Empty = acc
+foldSet _ fc2 _ (Singleton b) = fc2 b
+foldSet acc fc2 fc3 (Union a b) = fc3 (foldSet acc fc2 fc3 a) (foldSet acc fc2 fc3 b)
+
+
+isInF :: Eq a => a -> Set a -> Bool
+isInF a = foldSet False (a==) (||)
+
+subsetF :: Eq a => Set a -> Set a -> Bool
+subsetF set1 set2 = foldSet True (`isInF` set2) (&&) set1
