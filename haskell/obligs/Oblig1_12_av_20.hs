e9203271c04c467e443b252958f3beae6499e2f5
h671420 <671420@stud.hvl.no>
2024-10-13 20:59:52 +0200

    asd

--- haskell/obligs/Oblig1_12_av_20.hs
+++ haskell/obligs/Oblig1_12_av_20.hs
@@ -13,7 +13,7 @@ tokenise str
     | head str == '(' = [head str]:tokenise (tail str)
     | head str == ')' = [head str]:tokenise (tail str)
     | isDigit (head str) = takeWhile isDigit str : tokenise (dropWhile isDigit str) 
-    | isLetter (head str) = takeWhile isLetter str : tokenise (dropWhile isLetter str) 
+    | isLetter (head str) = takeWhile isDigit str : tokenise (dropWhile isDigit str) 
     | otherwise = tokenise (tail str)
     
 data Op = Add | Sub | Mult | Div deriving (Eq, Show)
@@ -33,7 +33,6 @@ parseTerm :: [String] -> (Ast , [String])
 parseTerm (token:tokens)
     | token == "(" = parseExpr (reverse (drop 1 (reverse tokens)) )
     | and [isNumber c|c<-token] = (Tall (read token::Int) , tokens)
-    | and [isLetter c|c<-token] = (Var token , tokens)
 
 --spm3
 parseFactor :: [String] -> (Ast , [String])
@@ -131,7 +130,6 @@ eval (BinOp op v h )
 --spm8
 ppInfix :: Ast -> String
 ppInfix (Tall t) = show t
-ppInfix (Var v) = v
 ppInfix (BinOp op v h )
     | op == Add = '(':(ppInfix v) ++ " + " ++(ppInfix h)++")"
     | op == Sub = '(':(ppInfix v) ++ " - " ++ (ppInfix h)++")"
@@ -141,7 +139,6 @@ ppInfix (BinOp op v h )
  --spm9
 ppPN :: Ast -> String 
 ppPN (Tall t) = show t
-ppPN (Var v) = v
 ppPN (BinOp op v h )
     | op == Add = "+ " ++ (ppPN v) ++ ' ':(ppPN h)
     | op == Sub = "- " ++ (ppPN v) ++ ' ':(ppPN h)
@@ -152,25 +149,44 @@ ppPN (BinOp op v h )
  --spm10
 ppOPN :: Ast -> String
 ppOPN (Tall t) = show t
-ppOPN (Var v) = v
 ppOPN (BinOp op v h )
     | op == Add = (ppOPN v) ++ ' ':(ppOPN h) ++" +" 
     | op == Sub = (ppOPN v) ++ ' ':(ppOPN h) ++" -" 
     | op == Mult = (ppOPN v) ++ ' ':(ppOPN h) ++" *" 
     | op == Div = (ppOPN v) ++ ' ':(ppOPN h) ++" /" 
 
-findVar :: [(String,Int)] -> String -> Int
-findVar pairs key = head [value | (key', value)<- pairs, key' == key]
+getLeft :: [String] -> [String]
+getLeft = getLeft' 0
+    where
+        getLeft' :: Int -> [String] -> [String]
+        getLeft' _ [] = [] 
+        getLeft' level (x:xs)
+            | x == "(" = x : getLeft' (level + 1) xs  
+            | x == ")" = x : getLeft' (level - 1) xs  
+            | (x == "-" || x == "+") && level == 0 = []  
+            | otherwise = x : getLeft' level xs  
 
-evalVar :: Ast -> [(String,Int)] -> Int
-evalVar (Var v) pairs= findVar pairs v
-evalVar (Tall t) _= t
-evalVar (BinOp op v h ) pairs
-    | op == Add = (evalVar v pairs) + (evalVar h pairs)
-    | op == Sub = (evalVar v pairs) - (evalVar h pairs)
-    | op == Mult = (evalVar v pairs) * (evalVar h pairs)
-    | op == Div = (evalVar v pairs) `div` (evalVar h pairs)
+getOp :: [String] -> String
+getOp = getOp' 0
+    where
+        getOp' :: Int -> [String] -> String
+        getOp' _ [] = error "No operator found"  
+        getOp' level (x:xs)
+            | x == "(" = getOp' (level + 1) xs
+            | x == ")" = getOp' (level - 1) xs
+            | (x == "-" || x == "+") && level == 0 = x
+            | otherwise = getOp' level xs
 
+getRight :: [String] -> [String]
+getRight = getRight' 0
+    where
+        getRight' :: Int -> [String] -> [String]
+        getRight' _ [] = []  
+        getRight' level (x:xs)
+            | x == "(" = getRight' (level + 1) xs
+            | x == ")" = getRight' (level - 1) xs
+            | (x == "-" || x == "+") && level == 0 = xs  
+            | otherwise = getRight' level xs
 
 tokens = tokenise "(22-1)*2+8*2"
 
